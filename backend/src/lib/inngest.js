433a0b09695241c465347b261d61e0952fca3166
Aryan <1706.aryanjain@gmaill.com>
2025-11-08 22:52:25 +0530

    email fix

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -6,27 +6,29 @@ import { upsertStreamUser,deleteStreamUser } from './stream.js';
 export const inngest = new Inngest({ id: "questly" });
 
 const syncUser = inngest.createFunction(
-    {id:"sync-user"},
-    {event:'clerk/user.created' },
-    async({event})=>{
-        await connectDB();
-        const {id,email_address,first_name,last_name,image_url} = event.data
-        const newUser = {
-            clerkId:id,
-            email:email_address,
-            name:`${first_name || ""} ${last_name || ""}`,
-            profileImage:image_url,
-        }
-        await User.create(newUser);
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
 
-        await upsertStreamUser({
-        id: newUser.clerkId.toString(),
-        name: newUser.name,
-        image: newUser.profileImage,
-      });
-    },
-      
-)
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`,
+      profileImage: image_url,
+    };
+
+    await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
+  }
+);
 const deleteUserFromDB = inngest.createFunction(
     {id:"delete-user-from-db"},
     {event:'clerk/user.deleted' },
