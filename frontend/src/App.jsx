a547e0fdad72f8bd95af6f7425f44cc2b9765910
Aryan <1706.aryanjain@gmaill.com>
2025-11-15 22:01:38 +0530

    frontend_setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css';
-import {SignInButton, SignOutButton, UserButton,SignedIn,SignedOut} from "@clerk/clerk-react";
+import {SignInButton, SignOutButton, UserButton,SignedIn,SignedOut, useUser} from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/Homepage";
+import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
+
 // No need to wrap userbutton in signed in/signed out components
 function App() {
+  const {isSignedIn}=useUser()
   return (
     <>
-      <h1>Welcome to app</h1>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <button>Login</button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton/>
-      </SignedIn>
-      
-      <UserButton/>
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+
+      <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>} />
+    </Routes>
+    <Toaster toastOptions={{duration:3000}}/>
     </>
   )
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react"
+import toast from "react-hot-toast"
+const HomePage = () => {
+  return (
+    <div>
+        <SignedOut>
+            <SignInButton mode="modal">
+                <button className="btn btn-secondary" onClick={()=> toast.success("This is success host")}>Login</button>
+            </SignInButton>
+        </SignedOut>
+        <SignedIn>
+            <SignOutButton/>
+        </SignedIn>
+        <UserButton/>
+    </div>
+  )
+}
+
+export default HomePage
