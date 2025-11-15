4cde9b0150a0d32f822748827a11bf84bc84f71e
Aryan <1706.aryanjain@gmaill.com>
2025-11-15 22:33:25 +0530

    Home page frontend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import {SignInButton, SignOutButton, UserButton,SignedIn,SignedOut, useUser} fro
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/Homepage";
 import ProblemsPage from "./pages/ProblemsPage";
+import DashboardPage from "./pages/DashboardPage";
 import { Toaster } from "react-hot-toast";
 
 // No need to wrap userbutton in signed in/signed out components
 function App() {
-  const {isSignedIn}=useUser()
+  const {isSignedIn,isLoaded}=useUser()
+  if(!isLoaded) return null;
   return (
     <>
     <Routes>
-      <Route path='/' element={<HomePage />}/>
-
+      <Route path='/' element={!isSignedIn?<HomePage /> :<Navigate to={"/dashboard"}/> }/>
+      <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"}/>} />
       <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>} />
     </Routes>
     <Toaster toastOptions={{duration:3000}}/>
